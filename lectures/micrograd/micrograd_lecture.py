79ca116a33f5a07f0a607aa167dd9f445c934ca9
adamjholt <ajh-git@pm.me>
2024-11-03 18:35:17 +0800

    micrograd lecture

---
+++ lectures/micrograd/micrograd_lecture.py
@@ -0,0 +1,26 @@
+# %%
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+
+def f(x):
+    return 3*x**2 - 4*x + 5
+
+# %%
+
+f(3.0)
+
+# %%
+
+xs = np.arange(-5, 5, 0.25)
+ys = f(xs)
+plt.plot(xs, ys)
+plt.show()
+
+# %%
+
+
+
