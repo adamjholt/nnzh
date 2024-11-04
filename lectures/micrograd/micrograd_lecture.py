4b4f3dffeda16c4ccc3842dc76981f169f701f46
adamjholt <ajh-git@pm.me>
2024-11-04 13:47:47 +0800

    micrograd lecture

--- lectures/micrograd/micrograd_lecture.py
+++ lectures/micrograd/micrograd_lecture.py
@@ -22,5 +22,38 @@ plt.show()
 
 # %%
 
+h = 0.000001
+x = 2/3
+(f(x + h) - f(x))/h
 
+# %%
+
+a = 2.0
+b = -3.0
+c = 10.0
+d = a*b + c
+print(d)
+
+# %%
+
+h = 0.0001
+
+# inputs
+a = 2.0
+b = -3.0
+c = 10.0
+
+d1 = a*b + c
+c += h
+d2 = a*b + c
+
+print('d1', d1)
+print('d2', d2)
+print('slope', (d2 - d1)/h)
+
+# %%
+
+# 00:19:09 starting the core Value object of micrograd and its visualization
+
+# %%
 
