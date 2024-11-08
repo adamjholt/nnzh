739eac67083f6922f36a7fc826858d64873f534c
adamjholt <ajh-git@pm.me>
2024-11-08 09:11:33 +0800

    add DAG viz function

--- lectures/micrograd/micrograd_lecture.py
+++ lectures/micrograd/micrograd_lecture.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 
 # %%
 
+
 def f(x):
-    return 3*x**2 - 4*x + 5
+    return 3 * x**2 - 4 * x + 5
+
 
 # %%
 
@@ -23,15 +25,15 @@ plt.show()
 # %%
 
 h = 0.000001
-x = 2/3
-(f(x + h) - f(x))/h
+x = 2 / 3
+(f(x + h) - f(x)) / h
 
 # %%
 
 a = 2.0
 b = -3.0
 c = 10.0
-d = a*b + c
+d = a * b + c
 print(d)
 
 # %%
@@ -43,19 +45,20 @@ a = 2.0
 b = -3.0
 c = 10.0
 
-d1 = a*b + c
+d1 = a * b + c
 c += h
-d2 = a*b + c
+d2 = a * b + c
 
-print('d1', d1)
-print('d2', d2)
-print('slope', (d2 - d1)/h)
+print("d1", d1)
+print("d2", d2)
+print("slope", (d2 - d1) / h)
+
 
 # %%
 
-class Value:
 
-    def __init__(self, data, _children=(), _op=''):
+class Value:
+    def __init__(self, data, _children=(), _op=""):
         self.data = data
         self._prev = set(_children)
         self._op = _op
@@ -64,12 +67,13 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        out = Value(self.data + other.data, (self, other), '+')
+        out = Value(self.data + other.data, (self, other), "+")
         return out
 
     def __mul__(self, other):
-        out = Value(self.data * other.data, (self, other), '*')
+        out = Value(self.data * other.data, (self, other), "*")
         return out
+
 
 # %%
 
@@ -77,7 +81,7 @@ a = Value(2.0)
 b = Value(-3.0)
 c = Value(10.0)
 
-d = a*b + c
+d = a * b + c
 
 d._prev
 d._op
@@ -86,13 +90,49 @@ d._op
 # %%
 
 
+from graphviz import Digraph
 
+
+def trace(root):
+    # builds a set of all nodes and edges in a graph
+    nodes, edges = set(), set()
+
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+
+    build(root)
+    return nodes, edges
+
+
+def draw_dot(root):
+    dot = Digraph(format="svg", graph_attr={"rankdir": "LR"})  # LR = left to right
+
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        # for any value in the graph, create a rectangular ('record') node for it
+        dot.node(
+            name=uid,
+            label="{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad),
+            shape="record",
+        )
+        if n._op:
+            # if this value is a result of some operation, create an op node for it
+            dot.node(name=uid + n._op, label=n._op)
+            # and connect this node to it
+            dot.edge(uid + n._op, uid)
+
+    for n1, n2 in edges:
+        # connect n1 to the op node of n2
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
 
 
 # %%
-
-
-
-
 
 
